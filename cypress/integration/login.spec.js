f6d2cd1bccb47687913d4e1d44a87aacc7a2b5e1
Walmyr <wlsf82@gmail.com>
2021-08-21 00:36:53 +0200

    Move login logic to a custom cmd

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,7 @@
 it('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
 
-  cy.visit('/login')
-  cy.get('#email').type(Cypress.env('USER_EMAIL'))
-  cy.get('#password').type(Cypress.env('USER_PASSWORD'), { log: false })
-  cy.contains('button', 'Login').click()
+  cy.login()
 
   cy.wait('@getNotes')
-  cy.contains('h1', 'Your Notes').should('be.visible')
 })
