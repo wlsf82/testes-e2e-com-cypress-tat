6bf1c7866d4eb61ce0cb34e026153b002cdb8f03
Walmyr <wlsf82@gmail.com>
2021-08-21 19:38:23 +0200

    Break lint and tests on purpose

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -34,13 +34,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
 
   it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
-    cy.wait('@getNotes')
-
-    if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
-      cy.get('.navbar-toggle.collapsed')
-        .should('be.visible')
-        .click()
-    }
+    cy.wait(2000)
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.nav > :nth-child(2) > a').click()
