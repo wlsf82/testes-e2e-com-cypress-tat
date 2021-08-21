26cf64ecb8d933dd2afe42064e4ed8e76401d085
Walmyr <wlsf82@gmail.com>
2021-08-21 19:41:43 +0200

    Fix lint issue

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -34,7 +34,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
 
   it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
-    cy.wait(2000)
+    cy.wait('@getNotes')
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.nav > :nth-child(2) > a').click()
