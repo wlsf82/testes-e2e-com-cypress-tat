53f8e21c2a6ca5fee98be5f5cf34c2c40da51e9c
Walmyr <wlsf82@gmail.com>
2021-08-21 16:10:15 +0200

    Add test for settings flow

---
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -0,0 +1,34 @@
+describe('Scenarios where authentication is a pre-requirement', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.login()
+  })
+
+  it('CRUDs a note', () => {
+    const faker = require('faker')
+    const noteDescription = faker.lorem.words(4)
+
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
+
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
+
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
+
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
+  })
+
+  it('successfully submits the form', () => {
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+
+    cy.fillSettingsFormAndSubmit()
+
+    cy.wait('@getNotes')
+    cy.wait('@paymentRequest').then(response => {
+      expect(response.state).to.equal('Complete')
+    })
+  })
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,4 @@
 import 'cypress-file-upload'
+import 'cypress-iframe'
 import 'cypress-mailosaur'
 import './commands'
