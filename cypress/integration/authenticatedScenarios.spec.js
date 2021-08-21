0f5133edf06425ad18fdd7ed930bc58a16dda46a
Walmyr <wlsf82@gmail.com>
2021-08-21 16:28:49 +0200

    Allow logout flow to be tested on tablet viewport

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -35,6 +35,13 @@ describe('Scenarios where authentication is a pre-requirement', () => {
   it('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes')
+
+    if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
+      cy.get('.navbar-toggle.collapsed')
+        .should('be.visible')
+        .click()
+    }
+
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.nav > :nth-child(2) > a').click()
     cy.get('#email').should('be.visible')
