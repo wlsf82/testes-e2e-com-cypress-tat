cc042c3cae6686efea28fb407140feab0138ffca
Walmyr <wlsf82@gmail.com>
2021-08-21 16:24:27 +0200

    Add test for logout flow
    With the help of Cypress Studio.

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -31,4 +31,13 @@ describe('Scenarios where authentication is a pre-requirement', () => {
       expect(response.state).to.equal('Complete')
     })
   })
+
+  it('logs out', () => {
+    cy.visit('/')
+    cy.wait('@getNotes')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.nav > :nth-child(2) > a').click()
+    cy.get('#email').should('be.visible')
+    /* ==== End Cypress Studio ==== */
+  })
 })
